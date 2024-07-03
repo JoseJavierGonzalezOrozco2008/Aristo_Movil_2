2188cdedfb9082adc06bcfb4532330a7e4beec09
maxsevictor <victor.hernandez@maxse.com.mx>
2024-07-03 10:19:42 -0600

    generica adapter con margenes

--- app/src/main/java/com/example/aristomovil2/adapters/GenericaAdapter.java
+++ app/src/main/java/com/example/aristomovil2/adapters/GenericaAdapter.java
@@ -11,14 +11,18 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.aristomovil2.Acrividades.AristoConfig;
 import com.example.aristomovil2.Acrividades.Carrito;
 import com.example.aristomovil2.Acrividades.Cobropv;
+import com.example.aristomovil2.Acrividades.Devolucion;
 import com.example.aristomovil2.Acrividades.Recargas;
 import com.example.aristomovil2.ActividadBase;
+import com.example.aristomovil2.BuscaProd;
 import com.example.aristomovil2.Inventario;
 import com.example.aristomovil2.R;
 import com.example.aristomovil2.modelos.Generica;
 import com.example.aristomovil2.utileria.Enumeradores;
+import com.example.aristomovil2.utileria.Libreria;
 
 import java.text.MessageFormat;
 import java.util.List;
@@ -36,6 +40,10 @@ public class GenericaAdapter extends BaseAdapter {
         switch(opcion){
             case 1:
                 v_xml=R.layout.item_buton;
+                break;
+            case 2:
+            case 8:
+                v_xml=R.layout.item_carrito;
                 break;
         }
     }
@@ -85,11 +93,20 @@ public class GenericaAdapter extends BaseAdapter {
 
                 break;
             case 2:
+                TextView t1=view.findViewById(R.id.item_producto);
+                TextView t2=view.findViewById(R.id.item_totalren);
+                t2.setVisibility(View.GONE);
+                t1.setText(gen.getTex1());
+                t1.setTextColor(base.getResources().getColor(R.color.black));
+                view.setOnClickListener(view1 -> accion(gen));
+                break;
             case 3:
             case 4:
             case 5:
             case 6:
             case 7:
+            case 9:
+            case 10:
                 TextView info=view.findViewById(R.id.acomid);
                 info.setText(gen.getTex2());
                 info.setTypeface(Typeface.MONOSPACE);
@@ -101,34 +118,67 @@ public class GenericaAdapter extends BaseAdapter {
                 (view.findViewById(R.id.acomDIfolio)).setVisibility(View.GONE);
                 (view.findViewById(R.id.acomubica)).setVisibility(View.GONE);
                 view.findViewById(R.id.acoAction1).setVisibility(View.GONE);
-                if(opcion!=7)
-                    info.setOnClickListener(v->{
-                        String titulo="Imprime ticket";
-                        String Mensaje = "";
-                        String boton = "Imprimir";
-                        String pantalla = "Pantalla";
-                        switch (opcion){
-                            case 5:
-                                titulo="Continuar con la venta";
-                                Mensaje=MessageFormat.format("¿Seguro de bajar la venta con folio {0}?",gen.getTex1());
-                                boton = "Continuar";
-                                break;
-                            default:
-                                Mensaje=MessageFormat.format("¿Seguro que volver a imprimir el folio {0}?",gen.getTex1());
+                info.setOnClickListener(v->{
+                    if(opcion==10){
+                        if(base instanceof Devolucion){
+                            ((Devolucion)base).agregaPorDevol(gen);
                         }
-                        final AlertDialog.Builder dialog = new AlertDialog.Builder(base);
-                        dialog.setTitle(titulo);
-                        dialog.setMessage(Mensaje);
+                        return;
+                    }
+                    String titulo="Imprime ticket";
+                    String Mensaje = "";
+                    String boton = "Imprimir";
+                    String pantalla = "Pantalla";
+                    boolean activo = true;
+                    switch (opcion){
+                        case 5:
+                            titulo="Continuar con la venta";
+                            Mensaje=MessageFormat.format("¿Seguro de bajar la venta con folio {0}?",gen.getTex1());
+                            boton = "Continuar";
+                            pantalla ="";
+                            break;
+                        case 7:
+                            titulo="Gestión del catalogo del producto";
+                            Mensaje=MessageFormat.format("Que quiere hace a continuacion con el producto  \n {0}",gen.getTex3());
+                            boton = "Editar";
+                            pantalla = "";
+                            activo = gen.getLog1()!=null ? !gen.getLog1() : true;
+                            break;
+                        case 10:
+                            titulo="Gestión del catalogo del producto";
+                            Mensaje=MessageFormat.format("Qué quiere hace a continuación con el producto  \n {0}",gen.getTex3());
+                            boton = "Devolver";
+                            pantalla = "";
+                            activo = gen.getLog1()!=null ? !gen.getLog1() : true;
+                            break;
+                        default:
+                            Mensaje=MessageFormat.format("¿Seguro que volver a imprimir el folio {0}?",gen.getTex1());
+                    }
+                    final AlertDialog.Builder dialog = new AlertDialog.Builder(base);
+                    dialog.setTitle(titulo);
+                    dialog.setMessage(Mensaje);
+                    if(Libreria.tieneInformacion(pantalla)){
                         dialog.setNegativeButton(pantalla,(dialogInterface, i1) -> {
                             base.aPantalla(true);
                             accion(gen);
                         });
+                    }
+                    if(activo){
                         dialog.setPositiveButton(boton, (dialogInterface, i2) -> {
+                            base.aPantalla(false);
                             accion(gen);
                         });
-                        dialog.setNeutralButton("Regresar", (dialogInterface, i2) -> {dialogInterface.dismiss();});
-                        dialog.show();
-                    });
+                    }
+                    dialog.setNeutralButton("Regresar", (dialogInterface, i2) -> {dialogInterface.dismiss();});
+                    dialog.show();
+                });
+                break;
+            case 8:
+                TextView margen=view.findViewById(R.id.item_producto);
+                TextView valor=view.findViewById(R.id.item_totalren);
+                margen.setText(gen.getTex1());
+                valor.setText(gen.getTex2());
+                valor.setTextColor(base.getResources().getColor(R.color.black));
                 break;
         }
         view.setTag(gen);
@@ -138,24 +188,38 @@ public class GenericaAdapter extends BaseAdapter {
     public void accion(Generica pGen){
         switch (opcion){
             case 2:
-                ((Carrito)base).traeTicketVnta(pGen.getTex1());
+                //((Carrito)base).traeTicketVnta(pGen.getTex1());
+                //base.wsImprime(Enumeradores.Valores.TAREA_VNTAULTIMAVNTA,pGen.getTex1());
+                ((AristoConfig)base).eligeColonia(pGen);
                 break;
             case 3:
-                ((Carrito)base).traeTicketArqe(Enumeradores.Valores.TAREA_IMPRIMEARQE,pGen.getTex1());
+                base.wsImprime(Enumeradores.Valores.TAREA_IMPRIMEARQE,pGen.getTex1());
                 break;
             case 4:
                 //((Inventario)base).traeTicketDoc(pGen.getTex1());
+                base.wsImprime(Enumeradores.Valores.TAREA_IMPRIMEDOCS,pGen.getTex1());
                 break;
             case 5:
                 if (base instanceof Carrito) {
                     ((Carrito)base).vntaLimpia();
                     ((Carrito)base).traeVnta(pGen.getTex1());
                 }else if (base instanceof Cobropv) {
-                    //((Cobropv)base).asignaVntafolio(pGen.getTex1());
+                    ((Cobropv)base).asignaVntafolio(pGen.getTex1());
                 }
                 break;
             case 6:
-                ((Carrito)base).traeTicketArqe(Enumeradores.Valores.TAREA_IMPRIMERETIRO,pGen.getTex1());
+                base.wsImprime(Enumeradores.Valores.TAREA_IMPRIMERETIRO,pGen.getTex1());
+                break;
+            case 7:
+                ((BuscaProd)base).wsBuscaProd(pGen.getEnt1());
+                break;
+            case 9:
+                base.wsImprime(Enumeradores.Valores.TAREA_TIKCORTEMOVIL,pGen.getTex1());
+                break;
+            case 10:
+                if(base instanceof Devolucion){
+                    ((Devolucion)base).agregaPorDevol(pGen);
+                }
                 break;
         }
     }
